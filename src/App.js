fd449184ce5e77ccacabdad7874ed56c546d2fbe
Pan-Artur <pankovets.artur1@gmail.com>
2025-03-15 00:25:50 +0200

    starting project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Article } from './components/Articles/Article';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Перший рядок</h2>
+      <Article/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Articles/Article.jsx
@@ -0,0 +1,7 @@
+export const Article = () => {
+    return <article>
+        <h2>Рецепт млинців</h2>
+        <img src="https://images.unian.net/photos/2020_09/thumb_files/400_0_1599769599-6966.jpg?0.48031324338453296" alt="Photo of pancakes" />
+        <p>Млинці, які приготовлені з любов'ю.</p>
+    </article>
+};
